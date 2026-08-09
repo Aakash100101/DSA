2e5e8aeb994bdb0151a304dce916edffca609187
Aakash Pandey <pandeyaakash950@gmail.com>
2026-08-09 15:37:56 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (16.15%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen={}
+        for num in nums:
+            seen[num]=seen.get(num,0)+1
+
+        for num,count in seen.items():
+            if count==1:
+                return num
+        return -1        
+
+            
+
+
+        
