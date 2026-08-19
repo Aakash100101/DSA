c1bb13fbd43aeee34ccfe7fd38288654406f7b42
Aakash Pandey <pandeyaakash950@gmail.com>
2026-08-20 02:36:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (76.32%) - LeetSync

---
+++ 4384-elevator-requests-i/elevator-requests-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def elevatorRequests(self, n: int, requests: list[int]) -> int:
+        sum=abs(0-requests[0])
+        
+        for i in range(1,len(requests)):
+            sum+=abs(requests[i]-requests[i-1])
+
+        return sum    
+
+
+
+
+        
